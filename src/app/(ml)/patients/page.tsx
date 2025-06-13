5171a94fab2eed934ab8322e6873499db5b71038
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-06-13 15:49:49 -0400

    Initial commit for patient portal application.

---
+++ src/app/(ml)/patients/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Patients = () => {
+  return <h2>Patients Page</h2>;
+};
+
+export default Patients;
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { HomeIcon, File, UsersRound } from "lucide-react";
+import Link from "next/link";
+import NavButton from "./NavButton";
+import { ModeToggle } from "@/components/ModeToggle";
+
+const Header = () => {
+  return (
+    <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
+      <div className="flex h-8 items-center justify-between w-full">
+        <div className="flex items-center gap-2">
+          <NavButton href="/home" label="Home" icon={HomeIcon} />
+          <Link
+            href="/home"
+            className="flex justify-center items-center gap-2 ml-0"
+            title="Home"
+          >
+            <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">
+              MediLink Patient Portal
+            </h1>
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <ModeToggle />
+          <NavButton href="/appointments" label="Appointments" icon={File} />
+          <NavButton href="/patients" label="Patients" icon={UsersRound} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
